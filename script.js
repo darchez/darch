5e9c4a9d00f51ecccfe3b8750cf39e14cebc5656
Alfonso <alfonsodarchez@gmail.com>
2024-12-27 17:56:29 -0300

    Animaciones

---
+++ script.js
@@ -0,0 +1,10 @@
+// Scroll suave (sin cambios importantes, pero se mantiene para la funcionalidad)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
